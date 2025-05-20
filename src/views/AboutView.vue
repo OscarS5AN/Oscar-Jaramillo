493ed0db278d19995160bac8b87de9df01389e2f
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 17:09:11 -0500

    se añadio un parrafo al aboutview

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>
+      Mi nombre es Oscar Jaramillo y espero que me vaya Bien en la vida, en salud, en economia, en sabiduria, y en una buena estabilidad emocional y mental.
+    </p>
   </div>
 </template>
