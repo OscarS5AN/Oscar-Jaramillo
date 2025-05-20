32b2633a44f820bfbbad515a1dc1c793ec78afdf
Oscar Eduardo Jaramillo Nuñez <38515273@claro.com.co>
2025-05-20 17:22:40 -0500

    se actualizo la configuracion

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import VueSweetalert2 from 'vue-sweetalert2'
+import 'sweetalert2/dist/sweetalert2.min.css'
 
+// Configuración actualizada de Font Awesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faEdit, faTrash, faPlus, faBank } from '@fortawesome/free-solid-svg-icons'
 
-createApp(App).use(router).mount('#app')
+library.add(faEdit, faTrash, faPlus, faBank)
+
+const app = createApp(App)
+app.use(router)
+app.use(VueSweetalert2)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.mount('#app')
